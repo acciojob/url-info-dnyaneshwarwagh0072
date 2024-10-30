fdeccd4713b0154e97545e03f03cc2bb7878f7dd
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-10-31 00:18:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ let currentURL = location.href;
 // Get the length of the URL
 let urlLength = currentURL.length;
 
-// Display the length of the URL using alert
+// Display the length of the URL using aler
 alert("The length of the URL is: " + urlLength);
